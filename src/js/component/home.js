4eb7a62390ce1c1e0431bc4baadd56f25baa00e4
adriancastro626 <adriancastro626@icloud.com>
2021-03-18 00:57:57 +0000

    todolist

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -11,38 +11,43 @@ export function Todo() {
 			let newArray = [...task, userInput];
 			setTask(newArray);
 			setUserInput("");
+			setCount(count + 1);
 		}
 	};
 
-	function removeTask(id) {
-		const elimTask = [...task].filter(userInput => userInput.id !== id);
-
-		setTask(elimTask);
+	const removeTask = id => {
+		task.splice(id, 1);
+		setTask([...task]);
 		setCount(count - 1);
-	}
+	};
 
 	return (
 		<>
-			<div className="fondo">
-				<div className="container" className="edit">
-					<h1 className="title">Enter Your Next Task</h1>
-					<input
-						className="boton"
-						type="text"
-						requiered
-						value={userInput}
-						onChange={e => setUserInput(e.target.value)}
-						onKeyPress={e =>
-							e.key === "Enter" ? handleInfo() : ""
-						}
-					/>
+			<div className="card" id="mainCard">
+				<h1 id="header" className="card-header">
+					To Do List
+				</h1>
+				<input
+					className="mb-2 inputClass"
+					placeholder="Add Task"
+					type="text"
+					requiered
+					value={userInput}
+					onChange={e => setUserInput(e.target.value)}
+					onKeyPress={e => (e.key === "Enter" ? handleInfo() : "")}
+				/>
 
-					{task.map((final, i) => (
-						<p key={i} onClick={() => removeTask()}>
-							{final}
-						</p>
-					))}
-				</div>
+				{task.map((final, id) => (
+					<span className="card-subtitle mb-4 claseTask" key={id}>
+						{final}
+						<button id="botonDelete" onClick={() => removeTask(id)}>
+							<i class="far fa-trash-alt"></i>
+						</button>
+					</span>
+				))}
+				<p id="counter" className="ml-1">
+					{count} task's left{" "}
+				</p>
 			</div>
 		</>
 	);
